172634facc56f6f807b34f6492c5ffbcb2271c4a
Anvar <66783845+crecker05ru@users.noreply.github.com>
2021-05-20 19:18:56 +0300

    first block

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,183 +1,57 @@
 import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
+import './index.css'
+import icon from "../images/spichka-icon.png"
+import phoneIcon from "../images/phone-icon.png"
+import upsideDownTriangleDashed from "../images/upside-down-triangle-dashed.png"
+import upsideDownTriangle from "../images/upside-down-triangle.png"
+import plusSmall from "../images/plus-small.png"
+import triangleStandart from "../images/triangle-standart.png"
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+    < >
+      <div className="main-block">
+        <svg className="right-line" width="739" height="850" viewBox="0 0 739 850" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M366.5 -1.52588e-05C375 -114 739 -1.14441e-05 739 -1.14441e-05V850.001L0 788C147 675 138.5 601.5 368 632C458.182 643.985 529.758 609.029 583.426 562.173C687.957 470.907 696.434 326.853 599.064 227.982C587.964 216.711 561.318 194.095 547.777 185.916C428.936 114.134 358.447 108.007 366.5 -1.52588e-05Z" fill="white"/>
+        </svg>
+        <svg className="bottom-line" width="1440" height="919" viewBox="0 0 1440 919" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M-55.7783 229.686C-55.8752 218.163 -49.8836 207.444 -40.0172 201.489L109.001 111.558C208.294 51.6359 330.969 45.3166 435.893 94.7191L532.781 140.338C603.811 173.781 688.158 159.397 744.088 104.302C748.945 99.5173 754.969 96.0867 761.562 94.3509L1074.16 12.0489C1139.89 -5.25488 1209.28 -2.52486 1273.44 19.8887L1512.46 103.385C1551.54 117.037 1577.82 153.772 1578.14 195.166L1583.57 918.183H-49.9858L-55.7783 229.686Z" fill="white"/>
+        </svg>
+          <div className="header">
+                <div className="header-spichka">
+                  <h1 className="header-text">Спичка</h1>
+                  <div className="header-icon"><img src={icon} alt="icon" /></div>
+                </div>
+                <div className="portfolio-contacts_block">
+                  <div className="portfolio">Портфолио</div>
+                  <div className="contacts">Контакты</div>
+                </div>
+                <div className="phone-number-block">
+                  <img className="phone-icon" src={phoneIcon} alt="phone icon"></img>
+                  <div className="number">8 928 877 37 40</div>
+                </div>
+                <button className="contact-us">Связаться с нами</button>
+                <div className="triangle">
+                  <img className="upside-down-triangle-dashed" src={upsideDownTriangleDashed} alt="upside down "></img>
+                  <img className="upside-down-triangle" src={upsideDownTriangle} alt="upside down triangle"></img>
+                </div>
+                <img className="plus-small" src={plusSmall} alt="plus small"></img>
+                <img className="triangle-standart" src={triangleStandart} alt="triangle standart"></img>
+                <div className="header-tagline">
+                  <h1 className="header-tagline-h1">Digital-решения</h1>
+                  <h1 className="header-tagline-h1">для вашего бизнеса</h1>
+                </div>
+                <div className="services">
+                  <h6>SaaS E-commerce</h6>
+                  <h6>|</h6>
+                  <h6>Мобильные приложения</h6>
+                  <h6>|</h6>
+                  <h6>CRM-системы</h6>
+                </div>
+                <button className="leave-request_button">Оставьте заявку</button>
+          </div>
+      </div>
+    </>
   )
 }
 
